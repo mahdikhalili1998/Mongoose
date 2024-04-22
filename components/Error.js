a4521638891ba8f98bd56831c1a8a8d938ac4c84
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-04-22 12:44:25 +0330

    file added

---
+++ components/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Error() {
+  return (
+    <h2 className="mx-auto mt-[4rem] w-max rounded-lg bg-red-500 px-2 py-1 font-medium text-white">
+      Invalid data
+    </h2>
+  );
+}
+
+export default Error;
---
+++ pages/api/data/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { name } = req.body;
+    if (!name || name.length <= 3) {
+      res.status(422).json({ data: "invald data" });
+      return;
+    }
+    mongoose.connect(
+      "mongodb+srv://mahdi_lhj:Mahdikhalili1998@mahditest.qfstcje.mongodb.net/?retryWrites=true&w=majority&appName=mahdiTest",
+      () => {
+        console.log("connected to DB");
+      },
+    );
+    res.status(201).json(name);
+  }
+}
